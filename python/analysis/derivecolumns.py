583880e9ad422105e5422c113c30b3ecd5c3c164
Curtis Alexander <calex@calex.org>
2020-08-16 17:37:52 -0500

    finish derivecolumns for python files

--- python/analysis/derivecolumns.py
+++ python/analysis/derivecolumns.py
@@ -16,11 +16,11 @@ hungry = person.assign(hungry="yes")
 ## derive value ##
 ##################
 
-def sweet_tooth_and_vowel(st, n):
-    if st == "yes" and n[0].lower() in ("a", "e", "i", "o", "u"):
+def sweet_tooth_and_vowel(s):
+    if s["sweet_tooth"] == "yes" and s["name"][0].lower() in ("a", "e", "i", "o", "u"):
         return "yes"
     else:
         return "no"
 
 # in-place
-person["hungry"] = person.applymap(lambda x, y: sweet_tooth_and_vowel(sweet_tooth, name))
+person["hungry"] = person.apply(sweet_tooth_and_vowel, axis=1)
