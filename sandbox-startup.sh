7792297a60970b18aadbea0d92927f5e6486b0a6
Curtis Alexander <calex@calex.org>
2020-10-23 19:16:32 -0500

    rm

--- sandbox-startup.sh
+++ sandbox-startup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -p 8888:8888 -v "$PWD":/home/jovyan/work -e ODA_USER -e ODA_PASSWORD saspyrilia-sandbox start.sh jupyter lab
+docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/work -e ODA_USER -e ODA_PASSWORD saspyrilia-sandbox start.sh jupyter lab
